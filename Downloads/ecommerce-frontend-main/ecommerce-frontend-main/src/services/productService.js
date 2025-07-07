1cf2389411466fd7fc0897f8718cc576bcbf15f9
Tanisha Bagga <tanishabagga30@gmail.com>
2025-07-08 00:48:42 +0530

    Removed wrongly committed ecommerce folder

--- Downloads/ecommerce-frontend-main/ecommerce-frontend-main/src/services/productService.js
+++
@@ -1,7 +0,0 @@
-// src/services/productService.js
-
-import apiClient from "@/lib/apiClient";
-
-export const fetchProductWithFilter = (query = "") => {
-  return apiClient.get(`/api/product/get-all?${query}`);
-};
